13b15d04e95aadf860231490f14ae671da93a519
hangyi <343043880@qq.com>
2024-05-29 23:07:26 +0800

    feat

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+class MoobiusApi {}
+
+export default MoobiusApi;
---
+++ src/utils/http.ts
@@ -0,0 +1,196 @@
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+import { captureHttpSentry } from '@/lib/sentry';
+import type { Store } from '@/store';
+// eslint-disable-next-line import/no-cycle
+import { getStore } from '@/store';
+// eslint-disable-next-line import/no-cycle
+import type { UserToken } from '@/store/modules/user';
+
+export const SUCCESS_CODE = 10000;
+export const AUTHERROT_CODE = 10005;
+export const UNCONFIRMED_MSG = 'UNCONFIRMED';
+interface IRequestOptions extends AxiosRequestConfig {
+  showLoading?: boolean;
+  ignoreAuth?: boolean;
+  headers?: any;
+}
+export interface IResponseData {
+  code: number;
+  msg: string;
+  data: any;
+}
+export interface IResponse<T = IResponseData> extends AxiosResponse {
+  data: T;
+}
+
+class Axios {
+  private instance: AxiosInstance;
+
+  private isRefreshing = false;
+
+  private refreshSubscribers: ((token: string) => void)[] = [];
+
+  constructor() {
+    this.instance = axios.create({
+      timeout: 25000,
+      // withCredentials: true,
+      baseURL:
+        process.env.NODE_ENV === 'development'
+          ? process.env.NEXT_PUBLIC_API_BASEURL
+          : process.env.NEXT_PUBLIC_API_PRODUCTION_BASEURL,
+    });
+
+    this.setupInterceptors();
+  }
+
+  private setupInterceptors() {
+    this.instance.interceptors.request.use(
+      this.handleSuccessRequeset as any,
+      this.handleErrorRequeset,
+    );
+    this.instance.interceptors.response.use(
+      this.handleSuccessResponse as any,
+      this.handleErrorResponse,
+    );
+  }
+
+  private async handleSuccessRequeset(config: IRequestOptions) {
+    const store: Store = getStore()!;
+    const { ignoreAuth = false } = config;
+    if (!ignoreAuth) {
+      const userInfo = (await store.userlocalForage.getItem(
+        'userToken',
+      )) as UserToken;
+      config.headers.Authorization =
+        `${userInfo?.TokenType} ${userInfo?.AccessToken}` || ``;
+      config.headers['Auth-Origin'] =
+        await store.userlocalForage.getItem('loginType');
+    }
+    return config;
+  }
+
+  private handleErrorRequeset = (error: any) => {
+    return Promise.reject(error);
+  };
+
+  private handleSuccessResponse = (response: AxiosResponse) => {
+    const originalRequest = response.config;
+    // 成功请求
+    if (
+      response?.data?.code === SUCCESS_CODE ||
+      /success/i.test(response?.data?.msg) ||
+      response.status === 304 ||
+      response.status === 204 ||
+      (response.config.responseType === 'arraybuffer' &&
+        response.status === 200)
+    ) {
+      return Promise.resolve(response.data);
+    }
+    if (
+      response?.data?.code === AUTHERROT_CODE &&
+      !(response.config as IRequestOptions)?.ignoreAuth
+    ) {
+      if (!this.isRefreshing) {
+        this.isRefreshing = true;
+        // 发起请求刷新token
+        return this.refreshToken()
+          .then((userInfo) => {
+            this.isRefreshing = false;
+            this.onRefreshed(userInfo?.AccessToken);
+            // 更新token并重新发送之前被拦截的请求
+            originalRequest.headers.Authorization = `Bearer ${userInfo?.AccessToken}`;
+            return this.instance(originalRequest);
+          })
+          .catch((refreshError) => {
+            this.isRefreshing = false;
+            return Promise.reject(refreshError);
+          });
+      }
+      return new Promise((resolve) => {
+        this.subscribeTokenRefresh((newAccessToken) => {
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          resolve(this.instance(originalRequest));
+        });
+      });
+    }
+    toast.error(`
+      ${response.config.url!} : 
+      ${response?.data?.message || response?.data?.data || '请求失败'}
+    `);
+    captureHttpSentry({
+      url: response.config.url!,
+      method: response.config.method!,
+      status: response.data.code,
+      statusText: response.data.msg,
+    });
+    return Promise.reject(response);
+  };
+
+  private handleErrorResponse = (error: any) => {
+    if (error.code === 'ECONNABORTED') {
+      toast.error(
+        `
+        ${error.config.url!} : 
+        ${error.message}
+      `,
+        {
+          onClose: () => {
+            window.location.reload();
+          },
+        },
+      );
+    } else {
+      toast.error(
+        `
+        ${error.config.url!} : 
+        ${error.message}
+      `,
+      );
+    }
+    captureHttpSentry({
+      url: error.config.url!,
+      method: error.config.method!,
+      status: error.code,
+      statusText: error.msg,
+    });
+    return Promise.reject(error);
+  };
+
+  private refreshToken(): Promise<any> {
+    const store: Store = getStore()!;
+    // 发起请求刷新token
+    return store.user.fetchRefresh();
+  }
+
+  private subscribeTokenRefresh(callback: (token: string) => void) {
+    this.refreshSubscribers.push(callback);
+  }
+
+  private onRefreshed(newAccessToken: string) {
+    this.refreshSubscribers.map((callback) => callback(newAccessToken));
+  }
+
+  public async get<T = any>(
+    url: string,
+    params?: any,
+    options?: IRequestOptions,
+  ): Promise<IResponse<T>> {
+    return this.instance.get(url, {
+      params,
+      ...options,
+    });
+  }
+
+  public async post<T = any>(
+    url: string,
+    data?: any,
+    options?: IRequestOptions,
+  ): Promise<IResponse<T>> {
+    return this.instance.post(url, data, options);
+  }
+}
+
+export default new Axios();
